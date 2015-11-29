9027f288deb024cfa8cc91e724083d0aabd271b4
Moujan Anna <moujan@annawake.com>
2015-11-28 18:28:43 -0800

    Fix syntax error preventing dd command from running

--- script/minimize.sh
+++ script/minimize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -u
 
 echo "==> Zero out the free space to save space in the final image";
-dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed";
+dd if=/dev/zero of=/EMPTY bs=1m || echo "dd exit code $? is suppressed";
 rm -f /EMPTY;
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
