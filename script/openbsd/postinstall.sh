f9594916e86ec882dceffcfef051d30455e8c94b
Rickard von Essen <rickard.von.essen@gmail.com>
2015-11-30 21:04:03 +0100

    Fixed OpenBSD box naming and added hint about sudo vs. doas

--- script/openbsd/postinstall.sh
+++ script/openbsd/postinstall.sh
@@ -12,10 +12,12 @@ echo "==> Install curl, ca_root_nss and sudo";
 pkg_add -I curl;
 pkg_add -I ca_root_nss;
 
+# Use "doas" from 5.8
 if [ "$major_version" -le 5 && "$minor_version" -lt 8 ]; then
   pkg_add -I sudo;
+else
+  echo 'alias sudo="echo \"Use doas instead of sudo!\""' > /home/${SSH_USERNAME}/.profile
 fi
-# Else use "doas" from 5.8
 
 echo "==> Enable NFS";
 # As sharedfolders are not in defaults ports tree, we will use NFS sharing
