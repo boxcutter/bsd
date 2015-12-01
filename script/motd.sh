36b5bcc86fca85d116dca68669da2dc668a800f2
Rickard von Essen <rickard.von.essen@gmail.com>
2015-12-01 10:29:41 +0100

    Fixed motd for BSD

--- script/motd.sh
+++ script/motd.sh
@@ -6,11 +6,13 @@ date > /etc/vagrant_box_build_date
 echo "==> Customizing message of the day";
 MOTD_FILE=/etc/motd
 BANNER_WIDTH=64
+PLATFORM_OS=$(uname -s);
 PLATFORM_RELEASE=$(uname -r);
-PLATFORM_MSG=$(printf '%s' "$PLATFORM_RELEASE")
+PLATFORM_ARCH=$(uname -m);
+PLATFORM_MSG=$(printf '%s' "$PLATFORM_OS $PLATFORM_RELEASE $PLATFORM_ARCH")
 BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
-printf '%0.1s' "-"{1..64} > ${MOTD_FILE}
+printf '%0.64s' "-------------------------------------------------------------------------------------------------------------------------" > ${MOTD_FILE}
 printf '\n' >> ${MOTD_FILE}
 printf '%2s%-30s%30s\n' " " "${PLATFORM_MSG}" "${BUILT_MSG}" >> ${MOTD_FILE}
-printf '%0.1s' "-"{1..64} >> ${MOTD_FILE}
+printf '%0.64s' "-------------------------------------------------------------------------------------------------------------------------" >> ${MOTD_FILE}
 printf '\n' >> ${MOTD_FILE}
