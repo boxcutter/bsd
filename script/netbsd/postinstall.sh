751398f46106bfed8e08e60f5a31f338feb055fc
Rickard von Essen <rickard.von.essen@gmail.com>
2016-07-29 18:00:07 +0200

    NetBSD: Properly install sudo and set root password

--- script/netbsd/postinstall.sh
+++ script/netbsd/postinstall.sh
@@ -3,29 +3,12 @@
 . /etc/shrc
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
-echo "==> Set root password";
-sed -i -e 's/^root::0/root:$sha1$22526$CHHJ53UQ$oSPxmOJn0jKxlMWFea8p6KlTpHj\/:0/' /etc/master.passwd
-
 echo "==> Setup NTP";
 # Set the time correctly
 echo 'ntpdate=YES' >> /etc/rc.conf
 
-echo "==> Install curl and sudo";
-# Install sudo and curl
+echo "==> Install curl";
 pkg_add curl;
-pkg_add sudo;
-
-echo "==> Setup sudo";
-mkdir -p /usr/pkg/etc
-cat >>/usr/pkg/etc/sudoers << SUDOERS
-##
-## User privilege specification
-##
-root ALL=(ALL) ALL
-
-## Allow members of group wheel to execute any command without a password
-%wheel ALL=(ALL) NOPASSWD: ALL
-SUDOERS
 
 echo "==> Enable NFS";
 # As sharedfolders are not in defaults ports tree, we will use NFS sharing
